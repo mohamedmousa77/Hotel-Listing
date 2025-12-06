20c1105d0a4fdd6c4cec70078798827446b8bf8a
Mohamed Mousa <mohamed.mousa.contact@gmail.com>
2025-12-06 06:40:53 +0100

    Completed the Endpoints for hotel. Conducted a simple test. Finished the data moduling.

--- HotelListing.Api/Data/Hotel.cs
+++ HotelListing.Api/Data/Hotel.cs
@@ -6,4 +6,14 @@ public class Hotel
     public string Name { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
     public double Rating { get; set; }
+    public int CountryId { get; set; }
+    public Country? Country { get; set; }
+}
+
+public class Country
+{
+    public int CountryId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ShortName { get; set; } = string.Empty;
+    public IList<Hotel> Hotels { get; set; } = new List<Hotel>();
 }
