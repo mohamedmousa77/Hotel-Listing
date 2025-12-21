e6a9327758036575a87c1ed7c5748a9775afc600
Mohamed Mousa <mohamed.mousa.contact@gmail.com>
2025-12-21 06:46:09 +0100

    Added Filtering Models

--- HotelListing.Api.Application/Services/BookingServices.cs
+++ HotelListing.Api.Application/Services/BookingServices.cs
@@ -3,11 +3,11 @@ using AutoMapper.QueryableExtensions;
 using HotelListing.Api.Application.Contracts;
 using HotelListing.Api.Application.DTOs.Bookings;
 using HotelListing.Api.Common.Constants;
+using HotelListing.Api.Common.Enums;
 using HotelListing.Api.Common.Models.Extentions;
 using HotelListing.Api.Common.Models.Paging;
 using HotelListing.Api.Common.Results;
 using HotelListing.Api.Domain;
-using HotelListing.Api.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.Api.Application.Services;
---
+++ HotelListing.Api.Common/Models/Filtering/BaseFilterParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelListing.Api.Common.Models.Filtering;
+
+public abstract class BaseFilterParameters
+{
+    public string? Search { get; set; }
+    public string? SortBy { get; set; }
+    public bool IsSortDescending { get; set; } = true;
+}
