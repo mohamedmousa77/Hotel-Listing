3925f59234c762c80529e09a4894884574eb1f42
Mohamed Mousa <mohamed.mousa.contact@gmail.com>
2025-12-31 02:50:35 +0100

    Add Deprecation. Added new controller for testing purposes.

---
+++ HotelListing.Api/Controllers/CountriesV2Controller.cs
@@ -0,0 +1,27 @@
+﻿// #### This File just for testing purposes!! ####
+
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Tasks;
+
+namespace HotelListing.Api.Controllers;
+
+[Route("api/v{version:apiVersion}/countries")]
+[ApiController]
+[ApiVersion("2.0", Deprecated = true)]
+public class CountriesV2Controller : Controller
+{
+    [HttpGet]
+    public IActionResult GetCounties(
+        [FromQuery] int? pageNumber = 1,
+        [FromQuery] int? pageSize = 10)
+    {
+        return Ok(new
+        {
+            Version = "v2.0",
+            Message = "Enhanced countries endpoint with pagination... This is the deprecated version of the Countries API."
+            PageNumber = pageNumber,
+            PageSize = pageSize
+        });
+    }
+}
